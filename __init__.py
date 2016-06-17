90c0353058a93c4d6bcf165bd80cb47cd1010fd0
Byron Smith <me@byronjsmith.com>
2016-06-17 14:51:49 -0400

    Fresh start on pandoc reader.

--- __init__.py
+++ __init__.py
@@ -1,24 +1,6 @@
-from .readers import PandocReader
+from pelican.readers import BaseReader
+import pypandoc as pd
 from pelican import signals
 
-def initialize(pelican):
-    # get pandoc file types from settings
-    if 'PANDOC_FILES' in pelican.settings:
-        PandocReader.file_extensions.extend(pelican.settings['PANDOC_FILES'])
-
-    # get pandoc path from settings
-    if 'PANDOC_PATH' in pelican.settings:
-        PandocReader.pandoc[0] = pelican.settings['PANDOC_PATH']
-
-    # get pandoc arguments from settings
-    if 'PANDOC_ARGS' in pelican.settings:
-        PandocReader.pandoc.extend(pelican.settings['PANDOC_ARGS'])
-
-    # associate PandocReader with file types
-    for ext in PandocReader.file_extensions:
-        pelican.reader_classes[ext] = PandocReader
-
-    return
-
-def register():
-    signals.readers_init.connect(initialize)
+class PandocReader(BaseReader):
+    pass
